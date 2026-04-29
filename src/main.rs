7b9ee743dc3b2bff59ffa554c1afe885fe825211
DonizeteVida <donizetevida1998@outlook.com>
2026-04-29 23:31:01 +0200

    Add a hexdump-like debugger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use anyhow::{Context, Result};
 
+#[cfg(debug_assertions)]
+fn dump(bytes: &[u8]) {
+    for (index, byte) in bytes.as_chunks::<2>().0.iter().enumerate() {
+        if index % 8 == 0 {
+            println!();
+            print!("{:08x} ", index * 2)
+        }
+
+        print!("{:02x}", byte[1]);
+        print!("{:02x} ", byte[0]);
+    }
+
+    println!()
+}
+
 fn main() -> Result<()> {
     let rom = std::env::args().nth(1).context("ch8 rom not found")?;
+    let bytes = std::fs::read(rom)?;
 
-    println!("{rom}");
+    #[cfg(debug_assertions)]
+    dump(&bytes);
 
     Ok(())
 }
