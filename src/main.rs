0cf2b8086691a92c2ad3d6cef1aa1ed93af2cadd
DonizeteVida <donizetevida1998@outlook.com>
2026-04-29 22:45:14 +0200

    Receive rom name as argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let rom = std::env::args().nth(1).context("ch8 rom not found")?;
+
+    println!("{rom}");
+
+    Ok(())
 }
