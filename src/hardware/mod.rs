e81d38b2b804e086d5caa3d39ccad7a20c738e7f
DonizeteVida <donizetevida1998@outlook.com>
2026-05-01 23:52:07 +0200

    Add and function

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -137,6 +137,12 @@ impl Chip8 {
         self[x] = vx | vy
     }
 
+    pub fn and(&mut self, x: u8, y: u8) {
+        let vx = self[x];
+        let vy = self[y];
+        self[x] = vx & vy
+    }
+
     pub fn seti(&mut self, nnn: u16) {
         self.i = nnn
     }
