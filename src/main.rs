e99091b9a3c7afe73f58db4fd96cfef9fadc3901
DonizeteVida <donizetevida1998@outlook.com>
2026-05-01 20:17:00 +0200

    Remove global import

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ mod macros;
 mod util;
 mod window;
 
-use anyhow::Result;
-
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     let data = io::load_rom()?;
     let mut window = window::Window::new();
     let mut chip8 = hardware::Chip8::new(&data);
