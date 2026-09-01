c8372030646c28bd3861b91f738a233a97bb96e9
DonizeteVida <donizetevida1998@outlook.com>
2026-09-01 16:21:29 +0200

    Rename Cpu struct variable

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,42 @@ mod hardware;
 mod util;
 
 fn main() -> anyhow::Result<()> {
-    let mut chip8 = hardware::Cpu::new(util::io::load_rom()?);
-    let mut window = hardware::Window::new(chip8.width, chip8.height)?;
+    let mut cpu = hardware::Cpu::new(util::io::load_rom()?);
+    let mut window = hardware::Window::new(cpu.width, cpu.height)?;
     let mut audio = hardware::Audio::new()?;
 
     loop {
-        let (a, b) = chip8.fetch();
-        let (c, d) = chip8.fetch();
+        let (a, b) = cpu.fetch();
+        let (c, d) = cpu.fetch();
 
         match (a, b, c, d) {
             nibbles!(0, 0, E, 0) => window.clear()?,
-            nibbles!(0, 0, E, E) => chip8.ret(),
-            nibbles!(1, _, _, _) => chip8.jmp(joinibble!(b, c, d)),
-            nibbles!(2, _, _, _) => chip8.call(joinibble!(b, c, d)),
-            nibbles!(3, _, _, _) => chip8.eq(b, joinibble!(c d)),
-            nibbles!(4, _, _, _) => chip8.nenn(b, joinibble!(c d)),
-            nibbles!(5, _, _, 0) => chip8.skip(b, c),
-            nibbles!(6, _, _, _) => chip8.setx(b, joinibble!(c d)),
-            nibbles!(7, _, _, _) => chip8.addn(b, joinibble!(c d)),
-            nibbles!(8, _, _, 0) => chip8.stxy(b, c),
-            nibbles!(8, _, _, 1) => chip8.or(b, c),
-            nibbles!(8, _, _, 2) => chip8.and(b, c),
-            nibbles!(8, _, _, 3) => chip8.xor(b, c),
-            nibbles!(8, _, _, 4) => chip8.addx(b, c),
-            nibbles!(8, _, _, 5) => chip8.subx(b, c),
-            nibbles!(8, _, _, 6) => chip8.shir(b),
-            nibbles!(8, _, _, 7) => chip8.suby(b, c),
-            nibbles!(8, _, _, E) => chip8.shil(b),
-            nibbles!(9, _, _, 0) => chip8.nexy(b, c),
-            nibbles!(A, _, _, _) => chip8.seti(joinibble!(b, c, d)),
-            nibbles!(B, _, _, _) => chip8.jmpi(joinibble!(b, c, d)),
-            nibbles!(C, _, _, _) => chip8.rand(b, joinibble!(c d)),
-            nibbles!(D, _, _, _) => chip8.draw(b, c, d, |buffer| window.draw(buffer))?,
-            nibbles!(F, _, 0, 7) => chip8.getd(b),
-            nibbles!(F, _, 0, A) => chip8.getk(b, || window.get_key())?,
-            nibbles!(F, _, 1, 5) => chip8.setd(b),
-            nibbles!(F, _, 1, 8) => chip8.sets(b),
+            nibbles!(0, 0, E, E) => cpu.ret(),
+            nibbles!(1, _, _, _) => cpu.jmp(joinibble!(b, c, d)),
+            nibbles!(2, _, _, _) => cpu.call(joinibble!(b, c, d)),
+            nibbles!(3, _, _, _) => cpu.eq(b, joinibble!(c d)),
+            nibbles!(4, _, _, _) => cpu.nenn(b, joinibble!(c d)),
+            nibbles!(5, _, _, 0) => cpu.skip(b, c),
+            nibbles!(6, _, _, _) => cpu.setx(b, joinibble!(c d)),
+            nibbles!(7, _, _, _) => cpu.addn(b, joinibble!(c d)),
+            nibbles!(8, _, _, 0) => cpu.stxy(b, c),
+            nibbles!(8, _, _, 1) => cpu.or(b, c),
+            nibbles!(8, _, _, 2) => cpu.and(b, c),
+            nibbles!(8, _, _, 3) => cpu.xor(b, c),
+            nibbles!(8, _, _, 4) => cpu.addx(b, c),
+            nibbles!(8, _, _, 5) => cpu.subx(b, c),
+            nibbles!(8, _, _, 6) => cpu.shir(b),
+            nibbles!(8, _, _, 7) => cpu.suby(b, c),
+            nibbles!(8, _, _, E) => cpu.shil(b),
+            nibbles!(9, _, _, 0) => cpu.nexy(b, c),
+            nibbles!(A, _, _, _) => cpu.seti(joinibble!(b, c, d)),
+            nibbles!(B, _, _, _) => cpu.jmpi(joinibble!(b, c, d)),
+            nibbles!(C, _, _, _) => cpu.rand(b, joinibble!(c d)),
+            nibbles!(D, _, _, _) => cpu.draw(b, c, d, |buffer| window.draw(buffer))?,
+            nibbles!(F, _, 0, 7) => cpu.getd(b),
+            nibbles!(F, _, 0, A) => cpu.getk(b, || window.get_key())?,
+            nibbles!(F, _, 1, 5) => cpu.setd(b),
+            nibbles!(F, _, 1, 8) => cpu.sets(b),
             nibbles!(_, _, _, _) => std::panic!("Not Implemented: {:x}{:x}{:x}{:x}", a, b, c, d),
         }
 
